2c224fd816ee63b29d066e989a59ed3d116cb99a
arsalan poodat <amir.arsalan.poudat@gmail.com>
2024-12-30 01:01:58 +0330

    feat: create the getStart page

---
+++ src/pages/getStart/getStart.js
@@ -0,0 +1,32 @@
+
+const swiper = new Swiper('.swiper', {
+    loop: false, 
+    navigation: false, 
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    on: {
+      slideChange: function () {
+
+        const customNextBtn = document.getElementById('custom-next');
+        if (swiper.isEnd) {
+          customNextBtn.textContent = 'Get Started';
+        } else {
+          customNextBtn.textContent = 'Next';
+        }
+      },
+    },
+  });
+  
+
+  const customNextBtn = document.getElementById('custom-next');
+  
+
+  customNextBtn.addEventListener('click', () => {
+    if (swiper.isEnd) {
+      window.location.href = '../log-in/log-in.html'; 
+    } else {
+      swiper.slideNext(); 
+    }
+  });
+  
