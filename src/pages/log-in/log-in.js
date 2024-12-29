d612887de62f70f5b01e914d6c575854ffa40f95
arsalan poodat <amir.arsalan.poudat@gmail.com>
2024-12-30 01:02:51 +0330

    feat: create the log-in page

---
+++ src/pages/log-in/log-in.js
@@ -0,0 +1,30 @@
+const backBtn = document.getElementById("backToGetStart");
+const emailInput = document.getElementById("email");
+const passInput = document.getElementById("pass");
+const singInBtn = document.getElementById("singInBtn");
+const passEye = document.getElementById("passEye");
+
+passEye.addEventListener("click", () => {
+  if (passInput.type === "password") {
+    passInput.type = "text";
+  } else {
+    passInput.type = "password";
+  }
+});
+
+backBtn.addEventListener("click", () => {
+  window.location.href = "../getStart/getStart.html";
+});
+
+function singInColor() {
+  if (emailInput.value && passInput.value) {
+    singInBtn.classList.remove("bg-opacity-50");
+    singInBtn.disabled = false;
+  } else {
+    singInBtn.classList.add("bg-opacity-50");
+    singInBtn.disabled = true;
+  }
+}
+
+emailInput.addEventListener("input", singInColor);
+passInput.addEventListener("input", singInColor);
